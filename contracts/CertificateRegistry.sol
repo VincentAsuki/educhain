d00e693d9ead2b112141a1e027cbbd5cbd7a715b
VincentAsuki <vincentasuki@gmail.com>
2026-03-09 01:12:19 +0100

    Update CertificateRegistry.sol

--- contracts/CertificateRegistry.sol
+++ contracts/CertificateRegistry.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract CertificateRegistry {
+
+    struct Certificate {
+        string studentName;
+        string institution;
+        string course;
+        uint256 dateIssued;
+    }
+
+    mapping(bytes32 => Certificate) public certificates;
+
+    function issueCertificate(
+        bytes32 certId,
+        string memory studentName,
+        string memory institution,
+        string memory course
+    ) public {
+
+        certificates[certId] = Certificate(
+            studentName,
+            institution,
+            course,
+            block.timestamp
+        );
+    }
+
+    function verifyCertificate(bytes32 certId)
+        public
+        view
+        returns (Certificate memory)
+    {
+        return certificates[certId];
+    }
+}
